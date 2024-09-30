4519e16e98b870aef185fd0180a558b3ec177d3a
TakumiGotou <163030244+GotouTakumi@users.noreply.github.com>
2024-09-30 18:08:36 +0000

    asotiation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  get 'users/index'
-  get 'users/show'
+  
  devise_for :users
  root to: "homes#top"
  
    resources :books, only: [:index,]
+   resources :users, only: [:index, :show]
  
  devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
