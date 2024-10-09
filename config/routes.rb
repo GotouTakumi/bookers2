e908b27c3ad3f967f0d52c682f71518b3a3e0799
TakumiGotou <163030244+GotouTakumi@users.noreply.github.com>
2024-10-09 06:16:21 +0000

    booksindex

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
  devise_for :users
  root to: "homes#top"
  get 'home/about' => 'homes#index'
+ get 'users/home/:id' => 'users#show'
   
    resources :books, only: [:index,]
-   resources :users, only: [:index, :show]
+   resources :users, only: [:index,]
   
  
  devise_scope :user do
